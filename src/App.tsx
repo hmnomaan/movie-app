e19e73485c42bc16a440dc9712db4a8c9d5b09d3
hmnomaan <hasannoman31@gmail.com>
2025-05-31 18:13:22 +0800

    adding hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Search from "./components/Search";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [searchTerm, setsearchTerm] = useState("");
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className="pattern" />
+      <div className="wrapper" >
+      <header>
+        <img src="../public/hero.png" alt="hero-banner" />
+      </header>
+      <h1>
+          Find <span className="text-gradient">Movies</span> You'll Enjoy
+          Without the hassle.
+        </h1>
+     
+        
+        <Search searchTerm={searchTerm} setSearchTerm={setsearchTerm} />
+        <h1 className="text-white">{searchTerm}</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </main>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Search.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div className="">
+        <img src="./../../public/search.svg" alt="search" />
+        <input
+          type="text"
+          placeholder="Search Through THousands of Movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
